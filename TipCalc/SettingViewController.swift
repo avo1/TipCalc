6cb9d011805071178857c6bd44cf573bae9a3ac1
avo1 <voanhdung@gmail.com>
2015-11-03 18:55:26 +0700

    Code cleanup
    - disable “Paste” at billTextFiled as user may paste something funny
    and crash the app
    - fix bug when compare 2 doubles (add abs)
    - change the way to store the userPreferences in NSUserDefaults to
    Dictionary
    - add app icon

--- TipCalc/SettingViewController.swift
+++ TipCalc/SettingViewController.swift
@@ -9,66 +9,40 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-    
-    @IBOutlet weak var defaultPercentageSegment: UISegmentedControl!
-    var defaults = NSUserDefaults.standardUserDefaults()
-    
-    @IBOutlet weak var roundedTotalSwitch: UISwitch!
-    @IBOutlet weak var roundedTipSwitch: UISwitch!
-    @IBOutlet weak var themeSwitch: UISwitch!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Set the defaultPercentageSegment to the default value
-        defaultPercentageSegment.selectedSegmentIndex = defaults.integerForKey(PERCENT_KEY)
-        
-        if let settingString = defaults.objectForKey(PREFER_KEY) as! String! {
-            roundedTipSwitch.on = settingString[settingString.startIndex] == "1"
-            roundedTotalSwitch.on = settingString[settingString.startIndex.advancedBy(1)] == "1"
-            themeSwitch.on = settingString[settingString.startIndex.advancedBy(2)] == "1"
-        } else {
-            roundedTotalSwitch.on = false
-            roundedTipSwitch.on = false
-            themeSwitch.on = false
-        }
+  
+  @IBOutlet weak var defaultPercentageSegment: UISegmentedControl!
+  var defaults = NSUserDefaults.standardUserDefaults()
+  
+  @IBOutlet weak var roundedTotalSwitch: UISwitch!
+  @IBOutlet weak var roundedTipSwitch: UISwitch!
+  @IBOutlet weak var themeSwitch: UISwitch!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    if let userPreferences = defaults.dictionaryForKey(userPreferKey) {
+      defaultPercentageSegment.selectedSegmentIndex = userPreferences[percentageKey] as! Int!
+      roundedTipSwitch.on = userPreferences[isRoundedTipKey] as! Bool!
+      roundedTotalSwitch.on = userPreferences[isRoundedTotalKey] as! Bool!
+      themeSwitch.on = userPreferences[isDarkKey] as! Bool!
+    } else {
+      defaultPercentageSegment.selectedSegmentIndex = 0
+      roundedTipSwitch.on = false
+      roundedTotalSwitch.on = false
+      themeSwitch.on = false
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // Return the selected value
-    override func viewWillDisappear(animated: Bool) {
-        
-        defaults.setInteger(defaultPercentageSegment.selectedSegmentIndex, forKey: PERCENT_KEY)
-        
-        var settingString = ""
-        var c: Character?
-        // Save for Tip first, then total, then Theme
-        c = roundedTipSwitch.on ? "1" : "0"
-        settingString.append(c!)
-        
-        c = roundedTotalSwitch.on ? "1" : "0"
-        settingString.append(c!)
-        
-        c = themeSwitch.on ? "1" : "0"
-        settingString.append(c!)
-        
-        defaults.setObject(settingString, forKey: PREFER_KEY)
-        
-        defaults.synchronize()
-    }
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
+  }
+  
+  // Save the selected values
+  override func viewWillDisappear(animated: Bool) {
+    let userPreferences: [String : AnyObject] = [
+      percentageKey: defaultPercentageSegment.selectedSegmentIndex,
+      isRoundedTipKey: roundedTipSwitch.on,
+      isRoundedTotalKey: roundedTotalSwitch.on,
+      isDarkKey: themeSwitch.on
+    ]
+    defaults.setObject(userPreferences, forKey: userPreferKey)
+    defaults.synchronize()
+  }
+  
 }
--- TipCalc/TipViewController.swift
+++ TipCalc/TipViewController.swift
@@ -8,250 +8,263 @@
 
 import UIKit
 
-let PERCENT_KEY = "defaultPercentageIndex"
-let PRE_DATA_KEY = "prevUserInput"
-let DATETIME_KEY = "lastUserInputSavedAt"
-let PREFER_KEY = "userPreference"
+let preDataKey = "prevUserInput"
+let dateTimeKey = "lastUserInputSavedAt"
+let userPreferKey = "userPreference"
+
+// For Dictionary to save user's preferences
+let percentageKey = "defaultPercentageIndex"
+let isRoundedTipKey = "isRoundedTip"
+let isRoundedTotalKey = "isRoundedTotal"
+let isDarkKey = "isDarkTheme"
+
+
+class CustomUITextField: UITextField {
+  // Disable "paste" on textField as we dont want user enter funny string and crash the app
+  override func canPerformAction(action: Selector, withSender sender: AnyObject?) -> Bool {
+    if action == "paste:" {
+      return false
+    }
+    return super.canPerformAction(action, withSender: sender)
+  }
+}
 
 class TipViewController: UIViewController, UITextFieldDelegate {
-    let EPSILON = 1e-6 // To compare 2 floats, or doubles
+  
+  let epsilon = 1e-6 // To compare 2 floats, or doubles
+  
+  @IBOutlet weak var billLabel: CustomUITextField!
+  @IBOutlet weak var tipsLabel: UILabel!
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var tipSegment: UISegmentedControl!
+  @IBOutlet weak var smileyImage: UIImageView!
+  @IBOutlet weak var roundedTipLabel: UILabel!
+  @IBOutlet weak var roundedTotalLabel: UILabel!
+  
+  @IBOutlet weak var numPeopleLabel: UILabel!
+  @IBOutlet weak var amountPerLabel: UILabel!
+  
+  @IBOutlet weak var plusLabel: UILabel!
+  @IBOutlet weak var equalLabel: UILabel!
+  @IBOutlet weak var upButton: UIButton!
+  @IBOutlet weak var downButton: UIButton!
+  @IBOutlet weak var nPeopleLabel: UILabel!
+  @IBOutlet weak var amountLabel: UILabel!
+  
+  var isRoundedTip = false
+  var isRoundedTotal = false
+  var isUsingDarkTheme = false
+  let darkColor = UIColor(red: 0, green: 128/255, blue: 0, alpha: 1)
+  let lightColor = UIColor(red:180/255, green:238/255, blue:180/255, alpha:1)
+  
+  var defaults = NSUserDefaults.standardUserDefaults()
+  var tipPercentage = 0.15
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    //print("viewDidLoad")
+    // Do any additional setup after loading the view, typically from a nib.
     
-    @IBOutlet weak var billLabel: UITextField!
-    @IBOutlet weak var tipsLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipSegment: UISegmentedControl!
-    @IBOutlet weak var smileyImage: UIImageView!
-    @IBOutlet weak var roundedTipLabel: UILabel!
-    @IBOutlet weak var roundedTotalLabel: UILabel!
+    // Set the keyboard for the Bill become NumberPad with decimal point
+    billLabel.keyboardType = UIKeyboardType.DecimalPad
+    billLabel.text = ""
+    billLabel.becomeFirstResponder()
+    billLabel.delegate = self
     
-    @IBOutlet weak var numPeopleLabel: UILabel!
-    @IBOutlet weak var amountPerLabel: UILabel!
-    
-    @IBOutlet weak var plusLabel: UILabel!
-    @IBOutlet weak var equalLabel: UILabel!
-    @IBOutlet weak var upButton: UIButton!
-    @IBOutlet weak var downButton: UIButton!
-    @IBOutlet weak var nPeopleLabel: UILabel!
-    @IBOutlet weak var amountLabel: UILabel!
-    // #B4EEB4
-    var isUsingDarkTheme = false
-    let darkColor = UIColor(red: 0, green: 128/255, blue: 0, alpha: 1)
-    let lightColor = UIColor(red:180.0/255.0, green:238.0/255.0, blue:180.0/255.0, alpha:1.0)
-//    UIColor(red: 128/255, green: 1, blue: 128/255, alpha: 1)
-    
-    var defaults = NSUserDefaults.standardUserDefaults()
-    var tipPercentage = 0.15
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //print("viewDidLoad")
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // Set the keyboard for the Bill become NumberPad with decimal point
-        billLabel.keyboardType = UIKeyboardType.DecimalPad
-        billLabel.text = ""
-        billLabel.becomeFirstResponder()
-        billLabel.delegate = self
-        
-        // Load previous user input if timeDiff < 10min
-        if let userInputDatetime = defaults.objectForKey(DATETIME_KEY) as! NSDate! {
-            let elapsedTime = NSDate().timeIntervalSinceDate(userInputDatetime)
-            if elapsedTime < 600 { // 10 min
-                if let userInput = defaults.objectForKey(PRE_DATA_KEY) as! String! {
-                    billLabel.text = userInput
-                }
-            }
+    // Load previous user input if timeDiff < 10min
+    if let userInputDatetime = defaults.objectForKey(dateTimeKey) as! NSDate! {
+      let elapsedTime = NSDate().timeIntervalSinceDate(userInputDatetime)
+      if elapsedTime < 600 { // 10 min
+        if let userInput = defaults.objectForKey(preDataKey) as! String! {
+          billLabel.text = userInput
         }
+      }
+    }
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    // Load the default settings, and pervious data here
+    
+    // what happens for the 1st time use of this default value, when it has not been set?
+    if let userPreferences = defaults.dictionaryForKey(userPreferKey) {
+      tipSegment.selectedSegmentIndex = userPreferences[percentageKey] as! Int!
+      isRoundedTip = userPreferences[isRoundedTipKey] as! Bool!
+      isRoundedTotal = userPreferences[isRoundedTotalKey] as! Bool!
+      isUsingDarkTheme = userPreferences[isDarkKey] as! Bool!
+    } else {
+      tipSegment.selectedSegmentIndex = 0
+      isRoundedTip = false
+      isRoundedTotal = false
+      isUsingDarkTheme = false
+    }
+    roundedTipLabel.hidden = !isRoundedTip
+    roundedTotalLabel.hidden = !isRoundedTotal
+    
+    loadTheme(isUsingDarkTheme)
+    
+    // It will not trigger valueChanged by code, manually call it
+    tipSegmentValueChanged(tipSegment)
+    
+    //print("viewWillAppear")
+    //print(percentageIndex)
+  }
+  
+  // Dismiss the keypad when touch outside the billLabel
+  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+    // If there are multiple textField, can use this way to apply for all
+    // view.endEditing(true)
+    // super.touchesBegan(touches, withEvent: event)
+    
+    // If single textField then can use this way
+    // if the billLabel is empty, no sense to dismiss the keypad?
+    if billLabel.text != "" {
+      billLabel.resignFirstResponder()
+    }
+  }
+  
+  func displayTips() {
+    // First catch the billLabel is empty?
+    // in case user change the TipPercentage while the billLabel is empty
+    let billValue = billLabel.text == "" ? 0.0 :Double(billLabel.text!)!
+    var tipValue = tipPercentage * billValue
+    // Round tip number
+    if isRoundedTip {
+      tipValue = abs(tipValue - floor(tipValue)) < epsilon ? tipValue : floor(tipValue+1)
     }
     
-    override func viewWillAppear(animated: Bool) {
-        // Load the default settings, and pervious data here
-        
-        // what happens for the 1st time use of this default value, when it has not been set?
-        let percentageIndex =  defaults.integerForKey(PERCENT_KEY)
-        tipSegment.selectedSegmentIndex = percentageIndex
-        
-        if let settingString = defaults.objectForKey(PREFER_KEY) as! String! {
-            print(settingString)
-            roundedTipLabel.hidden = settingString[settingString.startIndex] == "0"
-            roundedTotalLabel.hidden = settingString[settingString.startIndex.advancedBy(1)] == "0"
-            isUsingDarkTheme = settingString[settingString.startIndex.advancedBy(2)] == "1"
-        } else {
-            roundedTipLabel.hidden = true
-            roundedTotalLabel.hidden = true
-            isUsingDarkTheme = false
-        }
-        loadTheme(isUsingDarkTheme)
-        
-        // TODO: dont know why but it doesn't trigger valueChanged when set by code :(
-        tipSegmentValueChanged(tipSegment)
-        
-        
-        //print("viewWillAppear")
-        //print(percentageIndex)
+    var totalBill = billValue + tipValue
+    // Round total bill
+    if isRoundedTotal {
+      totalBill = abs(totalBill - floor(totalBill)) < epsilon ? totalBill : floor(totalBill+1)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Format locale currency
+    let formatter = NSNumberFormatter()
+    formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+    formatter.locale = NSLocale.currentLocale()
+    //formatter.locale = NSLocale(localeIdentifier: "en_US")
+    
+    let people = Int(numPeopleLabel.text!)
+    tipsLabel.text = formatter.stringFromNumber(tipValue)
+    totalLabel.text = formatter.stringFromNumber(totalBill)
+    amountPerLabel.text = formatter.stringFromNumber(totalBill / Double(people!))
+  }
+  
+  func loadTheme(isDark: Bool) {
+    let bgColor: UIColor!
+    let textColor: UIColor!
+    if isDark {
+      bgColor = darkColor
+      textColor = lightColor
+    } else {
+      bgColor = lightColor
+      textColor = darkColor
     }
     
-    // Dismiss the keypad when touch outside the billLabel
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
-        // If there are multiple textField, can use this way to apply for all
-        // view.endEditing(true)
-        // super.touchesBegan(touches, withEvent: event)
-        
-        // If single textField then can use this way
-        // if the billLabel is empty, no sense to dismiss the keypad?
-        if billLabel.text != "" {
-            billLabel.resignFirstResponder()
-        }
+    self.view.backgroundColor = bgColor
+    self.billLabel.backgroundColor = bgColor
+    self.billLabel.textColor = textColor
+    self.billLabel.tintColor = textColor
+    self.tipSegment.tintColor = textColor
+    
+    self.plusLabel.textColor = textColor
+    self.tipsLabel.textColor = textColor
+    self.roundedTipLabel.textColor = textColor
+    
+    self.equalLabel.textColor = textColor
+    self.totalLabel.textColor = textColor
+    self.roundedTotalLabel.textColor = textColor
+    
+    self.numPeopleLabel.textColor = textColor
+    self.nPeopleLabel.textColor = textColor
+    self.upButton.backgroundColor = bgColor
+    self.downButton.backgroundColor = bgColor
+    
+    if isDark {
+      self.upButton.setImage(UIImage(named: "up-light"), forState: UIControlState.Normal)
+      self.downButton.setImage(UIImage(named: "down-light"), forState: UIControlState.Normal)
+    } else {
+      self.upButton.setImage(UIImage(named: "up-green"), forState: UIControlState.Normal)
+      self.downButton.setImage(UIImage(named: "down-green"), forState: UIControlState.Normal)
     }
     
-    func displayTips() {
-        // First catch the billLabel is empty?
-        // in case user change the TipPercentage while the billLabel is empty
-        let billValue = billLabel.text == "" ? 0.0 :Double(billLabel.text!)!
-        var tipValue = tipPercentage * billValue
-        // Round tip number
-        if roundedTipLabel.hidden == false {
-            tipValue = tipValue - floor(tipValue) < EPSILON ? tipValue : floor(tipValue+1)
-        }
-        
-        var totalBill = billValue + tipValue
-        // Round total bill
-        if roundedTotalLabel.hidden == false {
-            totalBill = totalBill - floor(totalBill) < EPSILON ? totalBill :floor(totalBill+1)
-        }
-        
-        
-        // Format locale currency
-        let formatter = NSNumberFormatter()
-        formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
-        formatter.locale = NSLocale.currentLocale()
-        //formatter.locale = NSLocale(localeIdentifier: "en_US")
-        
-        let people = Int(numPeopleLabel.text!)
-        tipsLabel.text = formatter.stringFromNumber(tipValue)
-        totalLabel.text = formatter.stringFromNumber(totalBill)
-        amountPerLabel.text = formatter.stringFromNumber(totalBill / Double(people!))
+    self.amountPerLabel.textColor = textColor
+    self.amountLabel.textColor = textColor
+  }
+  
+  // UITextField Delegate
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    // Now validate the input as the user typing
+    //print("user typing..." + string)
+    
+    // If user enter . at the very beginning, add 0 to become 0.
+    if textField.text == "" && string == "." {
+      textField.text = "0"
+      return true
     }
     
-    func loadTheme(isDark: Bool) {
-        let bgColor: UIColor!
-        let textColor: UIColor!
-        if isDark {
-            bgColor = darkColor
-            textColor = lightColor
-        } else {
-            bgColor = lightColor
-            textColor = darkColor
-        }
-        
-        self.view.backgroundColor = bgColor
-        self.billLabel.backgroundColor = bgColor
-        self.billLabel.textColor = textColor
-        self.billLabel.tintColor = textColor
-        self.tipSegment.tintColor = textColor
-        
-        self.plusLabel.textColor = textColor
-        self.tipsLabel.textColor = textColor
-        self.roundedTipLabel.textColor = textColor
-        
-        self.equalLabel.textColor = textColor
-        self.totalLabel.textColor = textColor
-        self.roundedTotalLabel.textColor = textColor
-        
-        self.numPeopleLabel.textColor = textColor
-        self.nPeopleLabel.textColor = textColor
-        self.upButton.backgroundColor = bgColor
-        self.downButton.backgroundColor = bgColor
-        
-        if isDark {
-            self.upButton.setImage(UIImage(named: "up-light"), forState: UIControlState.Normal)
-            self.downButton.setImage(UIImage(named: "down-light"), forState: UIControlState.Normal)
-        } else {
-            self.upButton.setImage(UIImage(named: "up-green"), forState: UIControlState.Normal)
-            self.downButton.setImage(UIImage(named: "down-green"), forState: UIControlState.Normal)
-        }
-        
-        self.amountPerLabel.textColor = textColor
-        self.amountLabel.textColor = textColor
-        
-    }
-    
-    // UITextField Delegate
-    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        // Now validate the input as the user typing
-        //print("user typing..." + string)
-        
-        // If user enter . at the very beginning, add 0 to becomce 0.
-        if textField.text == "" && string == "." {
-            textField.text = "0"
-            return true
-        }
-        
-        // Should not have leading 0, unless 0.x
-        if textField.text == "0" {
-            if string == "." {
-                return true
-            } else {
-                textField.text = ""
-            }
-        }
-        
-        // Should not have more than 1 '.'
-        if (textField.text!.containsString(".")) {
-            if string == "." {
-                return false
-            }
-        }
-        
+    // Should not have leading 0, unless 0.x
+    if textField.text == "0" {
+      if string == "." {
         return true
+      } else {
+        textField.text = ""
+      }
     }
     
-    @IBAction func billValueChanged(sender: UITextField) {
-        // Need to constantly save the userInput
-        let currentDateTime = NSDate()
-        //let formatter = NSDateFormatter()
-        //formatter.dateFormat = "ddMMyyyy-HHmmss"
-        defaults.setObject(currentDateTime, forKey: DATETIME_KEY)
-        defaults.setObject(billLabel.text, forKey: PRE_DATA_KEY)
-        
-        defaults.synchronize()
-        
-        displayTips()
+    // Should not have more than 1 '.'
+    if (textField.text!.containsString(".")) {
+      if string == "." {
+        return false
+      }
     }
     
-    @IBAction func tipSegmentValueChanged(sender: UISegmentedControl) {
-        switch tipSegment.selectedSegmentIndex {
-        case 0:
-            tipPercentage = 0.15
-            smileyImage.image = isUsingDarkTheme ? UIImage(named: "justOK-light") :UIImage(named: "justOK-green")
-        case 1:
-            tipPercentage = 0.18
-            smileyImage.image = isUsingDarkTheme ? UIImage(named: "good-light") :UIImage(named: "good-green")
-        case 2:
-            tipPercentage = 0.20
-            smileyImage.image = isUsingDarkTheme ? UIImage(named: "excellent-light") : UIImage(named: "excellent-green")
-        default: tipPercentage = 0.0  // This should never happen
-        }
-        displayTips()
-    }
+    return true
+  }
+  
+  @IBAction func billValueChanged(sender: UITextField) {
+    // Need to constantly save the userInput
+    let currentDateTime = NSDate()
+    //let formatter = NSDateFormatter()
+    //formatter.dateFormat = "ddMMyyyy-HHmmss"
+    defaults.setObject(currentDateTime, forKey: dateTimeKey)
+    defaults.setObject(billLabel.text, forKey: preDataKey)
+    defaults.synchronize()
     
-    @IBAction func increasePeople(sender: UIButton) {
-        let n = Int(numPeopleLabel.text!)
-        numPeopleLabel.text = "\(n!+1)"
-        displayTips()
+    displayTips()
+  }
+  
+  @IBAction func tipSegmentValueChanged(sender: UISegmentedControl) {
+    switch tipSegment.selectedSegmentIndex {
+    case 0:
+      tipPercentage = 0.15
+      smileyImage.image = isUsingDarkTheme ? UIImage(named: "justOK-light") :UIImage(named: "justOK-green")
+    case 1:
+      tipPercentage = 0.18
+      smileyImage.image = isUsingDarkTheme ? UIImage(named: "good-light") :UIImage(named: "good-green")
+    case 2:
+      tipPercentage = 0.20
+      smileyImage.image = isUsingDarkTheme ? UIImage(named: "excellent-light") : UIImage(named: "excellent-green")
+    default: tipPercentage = 0.0  // This should never happen
     }
-    
-    @IBAction func decreasePeople(sender: UIButton) {
-        let n = Int(numPeopleLabel.text!)
-        if n > 1 {
-            numPeopleLabel.text = "\(n!-1)"
-            displayTips()
-        }
+    displayTips()
+  }
+  
+  @IBAction func increasePeople(sender: UIButton) {
+    let n = Int(numPeopleLabel.text!)
+    if n < 100 {
+      numPeopleLabel.text = "\(n!+1)"
+      displayTips()
     }
+  }
+  
+  @IBAction func decreasePeople(sender: UIButton) {
+    let n = Int(numPeopleLabel.text!)
+    if n > 1 {
+      numPeopleLabel.text = "\(n!-1)"
+      displayTips()
+    }
+  }
+  
 }
 
